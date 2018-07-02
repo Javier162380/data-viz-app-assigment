96a479c76e8b358bbcba165feee1e11e8a1dbe80
Javier162380 <javierllorente16@gmail.com>
2018-07-03 01:32:27 +0200

    adding two varchars

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from datasets import international_transparency_data,international_transparency_pivot_data,\
-    all_kpis,spain_corruption
+    all_kpis,spain_corruption,standard_error,sources
 from static_graphs import spain_pie_graph, spain_corruption_graph
 import plotly.graph_objs as go
 
@@ -20,8 +20,11 @@ app.layout = html.Div(children=[
         value='',
         multi=True
     ),
-    html.Div([html.Div(id='Historical_Graph_Evolution'),
-              html.Div(id='Violin Graph')]),
+    html.Div(id='Historical_Graph_Evolution'),
+    html.H4(children='Sources'),
+    html.Div(id='Sources_Evolution'),
+    html.H4(children='CPI Standard Error'),
+    html.Div(id='Standard_Error_Evolution'),
     html.H4(children='International Transaparency Table'),
     html.Div(id='Continents_table'),
     html.H4(children='CPI Rank by Region'),
@@ -30,7 +33,7 @@ app.layout = html.Div(children=[
             id='Regions_Selector',
             options=[{'label': region, 'value': region}
                  for region in international_transparency_pivot_data.Region_Full_Name.values],
-            values='',
+            values=[region for region in international_transparency_pivot_data.Region_Full_Name.values],
             labelStyle={'display': 'inline-block'},
             style={'height': 100, 'width':'40%'}),
     html.Div(id='Continents_Scatter_Plot_Evolution'),
@@ -43,7 +46,7 @@ app.layout = html.Div(children=[
 @app.callback(
     dash.dependencies.Output('Historical_Graph_Evolution', 'children'),
     [dash.dependencies.Input('Country_Dropdown', 'value')])
-def update_graph(Country_Dropdown):
+def historical_graph(Country_Dropdown):
     Historical_Graph_Evolution = []
     Historical_Graph_Evolution.append(dcc.Graph(
         id='Historical Graph',
@@ -61,6 +64,51 @@ def update_graph(Country_Dropdown):
                     range=[0, 100],
                     dtick=10)}}))
     return Historical_Graph_Evolution
+
+
+@app.callback(
+    dash.dependencies.Output('Standard_Error_Evolution', 'children'),
+    [dash.dependencies.Input('Country_Dropdown', 'value')])
+def standard_graph(Country_Dropdown):
+    return dcc.Graph(
+        figure=go.Figure(
+            data=[
+                go.Bar(
+                    x=standard_error[standard_error['Country'] == country]['year'].tolist(),
+                    y=standard_error[standard_error['Country'] == country]['standard_error'].tolist(),
+                    name=country)
+                for country in Country_Dropdown
+            ],
+            layout=go.Layout(
+                title='CPI Standard Error',
+                showlegend=True,
+                margin=go.Margin(l=40, r=0, t=40, b=30)
+            )
+        ),
+        style={'height': 300},
+        id='standard_error_graph')
+
+@app.callback(
+    dash.dependencies.Output('Sources_Evolution', 'children'),
+    [dash.dependencies.Input('Country_Dropdown', 'value')])
+def sources_graph(Country_Dropdown):
+    return dcc.Graph(
+        figure=go.Figure(
+            data=[
+                go.Bar(
+                    x=sources[sources['Country']==country]['year'].tolist(),
+                    y=sources[sources['Country']==country]['sources'].tolist(),
+                    name=country)
+                for country in Country_Dropdown
+            ],
+            layout=go.Layout(
+                title='Sources',
+                showlegend=True,
+                margin=go.Margin(l=40, r=0, t=40, b=30)
+            )
+        ),
+        style={'height': 300},
+        id='sources_error_graph')
 
 def generate_table(dataframe, max_rows=10):
     return html.Table(
@@ -89,7 +137,7 @@ def display_table(dropdown_value):
 def scatter_plot(Regions_Selector):
     filter_df = international_transparency_pivot_data[
                 international_transparency_pivot_data['Region_Full_Name'].isin(Regions_Selector)]
-    return    dcc.Graph(
+    return  dcc.Graph(
                     id='life-exp-vs-gdp',
                     figure={
                             'data': [

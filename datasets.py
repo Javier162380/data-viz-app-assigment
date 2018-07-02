b1ace3f29001293b886411fa680195c2a2eb955c
Javier162380 <javierllorente16@gmail.com>
2018-07-03 01:32:56 +0200

    upload two new datasets

--- datasets.py
+++ datasets.py
@@ -1,4 +1,4 @@
-from pandas import read_csv,melt
+from pandas import read_csv,melt,DataFrame
 
 international_transparency_data = read_csv('data/international_transparency.csv')
 international_transparency_pivot_data = melt(international_transparency_data,
@@ -13,6 +13,7 @@ CPI_regions = {'AP':'Asia & the Pacific','AME':'America',
 
 def regions(acronym):
     return CPI_regions.get(acronym,None)
+
 international_transparency_pivot_data['Region_Full_Name']=(international_transparency_pivot_data['Region'].
                                                                                                 apply(regions))
 spain_corruption = read_csv('data/spain_corruption.csv')
@@ -21,5 +22,28 @@ def filter_year(date):
         return int(date[1:5])
     else:
         return int(date[0:4])
+
 spain_corruption['year'] = spain_corruption['fecha'].apply(filter_year)
 all_kpis = read_csv('data/kpis_related_with_transparency.csv')
+
+sources_kpis = read_csv('data/transparency_sources.csv', delimiter='\t',
+                        header=0,
+                        names=['Country', 'ISO3', 'Region', '2017', '2016',
+                                        '2015', '2014', '2013', '2012'])
+
+sources = melt(sources_kpis,
+               id_vars=['Country','ISO3','Region'])
+sources['variable'].astype(int)
+sources.rename(columns={'variable': 'year', 'value': 'sources'}, inplace=True)
+
+
+standard_error_kpis = read_csv('data/standard_error.csv', delimiter='\t',
+                        header=0,
+                        names=['Country', 'ISO3', 'Region', '2017', '2016',
+                                        '2015', '2014', '2013', '2012'])
+
+standard_error = melt(standard_error_kpis,id_vars=['Country','ISO3','Region'])
+standard_error['variable'].astype(int)
+standard_error['value'].astype(float)
+standard_error.rename(columns={'variable': 'year', 'value': 'standard_error'}, inplace=True)
+
